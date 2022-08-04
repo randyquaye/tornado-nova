baefdd08111be23495f34de485af3b1f7b375765
Randy Quaye <randyquaye10@gmail.com>
2022-08-04 15:18:33 +0100

    Updated UTXO type and modified circuits

--- src/utxo.js
+++ src/utxo.js
@@ -8,16 +8,18 @@ class Utxo {
    *
    * @param {BigNumber | BigInt | number | string} amount UTXO amount
    * @param {BigNumber | BigInt | number | string} blinding Blinding factor
+   * @param {BigNumber | BigInt | number | string} rand 2nd Blinding factor
    * @param {Keypair} keypair
    * @param {number|null} index UTXO index in the merkle tree
    */
   constructor({ amount = 0, type = randomBN(), keypair = new Keypair(), blinding = randomBN(), rand = randomBN(), index = null } = {}) {
     this.amount = BigNumber.from(amount)
     this.blinding = BigNumber.from(blinding)
-    this.type = BigNumber.from(type)
+    this.type = BigNumber.from(type) //encode ERC20 token address
     this.rand = BigNumber.from(rand)
     this.keypair = keypair
     this.index = index
+    console.log(this.blinding)
   }
 
   /**
@@ -27,8 +29,8 @@ class Utxo {
    */
   getCommitment() {
     if (!this._commitment) {
-      let temp  = poseidonHash([this.keypair.pubkey, this.blinding])
-      this._commitment = poseidonHash([temp, this.amount, this.type, this.rand])
+      let owner  = poseidonHash([this.keypair.pubkey, this.blinding])
+      this._commitment = poseidonHash([owner, this.amount, this.type, this.rand])
 
     }
     return this._commitment
@@ -84,6 +86,27 @@ class Utxo {
       keypair,
       index,
     })
+  }
+
+  /**
+   * Decrypt a UTXO
+   *
+   * @param {Keypair} keypair keypair used to decrypt
+   * @param {BigNumber} commitment hex string with data
+   * @returns {BigNumber}
+   */
+  static nullify(commitment, keypair, index){
+    if (
+      this.amount > 0 &&
+      (this.index === undefined ||
+        this.index === null ||
+        this.keypair.privkey === undefined ||
+        this.keypair.privkey === null)
+    ) {
+      throw new Error('Can not compute nullifier without utxo index or private key')
+    }
+    const signature = keypair.privkey ? keypair.sign(commitment,index || 0) : 0
+    this._nullifier = poseidonHash([commitment(), index || 0, signature])
   }
 }
 
